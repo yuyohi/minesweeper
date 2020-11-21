74c966a18bed1d1140441e4e086396e1f9b4d8c8
yuya <41873114+yuyohi@users.noreply.github.com>
2020-11-21 13:00:32 +0900

    initial

--- src/main/kotlin/minesweeper/game/GameUI.java
+++ src/main/kotlin/minesweeper/game/GameUI.java
@@ -6,7 +6,5 @@ public class GameUI {
     private JPanel minesweeper;
     private JPanel gameDisplay;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+
 }
